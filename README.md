222254a45148b44ff21acf5f50181c8eb780229b
toberole <zhouwei441300@gmail.com>
2019-01-28 17:05:59 +0800

    note

---
+++ README.md
@@ -0,0 +1,6 @@
+### asio
+work 可以是的执行run的线程在执行完毕任务之后不会退出
+
+post 将任务丢进任务队列, 任务会按照顺序的执行
+dispatch 及时请求io_service处理任务
+
--- src/demo2.cpp
+++ src/demo2.cpp
@@ -4,49 +4,50 @@
 #include "B.hpp"
 #include "C.hpp"
 
+#include <boost/asio.hpp>
+
 typedef void (*Func)(void);
-typedef int (*Func1)(int ,int);
 
-void test_demo02(){
-	printf("test_demo02\n");
+typedef int (*Func1)(int, int);
+
+void test_demo02() {
+    printf("test_demo02\n");
 }
 
-int testx_demo02(int a,int b){
-	return a+b;
+int testx_demo02(int a, int b) {
+    return a + b;
 }
 
-void test1_demo02(Func f){
-	Func1 f1 = (Func1)f;
-	int k = f1(1,2);
-	printf("test1_demo02 k = %d\n",k);
+void test1_demo02(Func f) {
+    Func1 f1 = (Func1) f;
+    int k = f1(1, 2);
+    printf("test1_demo02 k = %d\n", k);
 }
 
-typedef struct stu
-{
+typedef struct stu {
     int age;
     char s;
     long int i;
-    
-}Student;
 
+} Student;
 
+boost::asio::io_service io_service;
 
-int main(int argc, char const *argv[])
-{
-	// Func f = (Func)testx_demo02;
-	// test1_demo02(f);
+int main(int argc, char const *argv[]) {
+    // Func f = (Func)testx_demo02;
+    // test1_demo02(f);
 
 
     // printf("Student bytes = %d\n", sizeof(Student));
 
 
-	// int a = 11;
-	// int *p = &a;
-	// int **p1 = &p;
+    // int a = 11;
+    // int *p = &a;
+    // int **p1 = &p;
 
-	// printf("p = %p\n",p );
-	// printf("*p1 = %p\n",*p1 );
-
+    // printf("p = %p\n",p );
+    // printf("*p1 = %p\n",*p1 );
+/*
 	A a;
 	B b;
 	C c;
@@ -57,9 +58,39 @@ int main(int argc, char const *argv[])
 
 	// 动态多态 通过虚拟函数实现
 	a.hello_1();
+*/
 
-	
-	printf("press any key to exit......\n");
-	getchar();
-	return 0;
+
+    io_service.post([]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+        std::cout << "=== io_service.post ===" << std::endl;
+    });
+
+    boost::asio::io_service::work work(io_service);
+
+    std::thread th1([]() {
+        io_service.post([]() {
+            std::cout << "=== th1 io_service.post task === thread_id: " << std::this_thread::get_id() << std::endl;
+        });
+    });
+
+    std::thread th2([]() {
+        io_service.post([]() {
+            std::cout << "=== th2 io_service.post task === thread_id: " << std::this_thread::get_id() << std::endl;
+        });
+    });
+
+    std::cout << "=== main task === thread_id: " << std::this_thread::get_id() << std::endl;
+
+    io_service.run();
+
+//    io_service.poll();
+//    std::cout << "------ io_service.poll() after ------" << std::endl;
+//    io_service.poll();
+
+    std::cout << "io_service.run() after" << std::endl;
+
+    printf("press any key to exit......\n");
+    getchar();
+    return 0;
 }
